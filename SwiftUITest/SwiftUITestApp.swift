bf36d987fbbc733e452a7818d9bdcb6f50c07860
Walkramis <andreas.hoglind@gmail.com>
2021-09-28 20:33:48 +0200

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Andreas Hoglind on 2021-09-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
