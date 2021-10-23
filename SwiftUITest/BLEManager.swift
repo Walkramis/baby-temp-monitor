9837fd3a8fda218df70b33e66c10455df4a213a6
Walkramis <andreas.hoglind@gmail.com>
2021-10-23 20:08:20 +0200

    Working prototype test

--- SwiftUITest/BLEManager.swift
+++ SwiftUITest/BLEManager.swift
@@ -10,31 +10,42 @@ struct Peripheral: Identifiable {
 }
 
 class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
+    let uuidCharForIndicate = CBUUID(string: "25AE1444-05D3-4C5B-8281-93D4E07420CF")
+    let uuidService = CBUUID(string: "25AE1441-05D3-4C5B-8281-93D4E07420CF")
+    var selectedPeripheral: CBPeripheral!
 
     var myCentral: CBCentralManager!
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        var peripheralName: String!
-       
-        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
-            peripheralName = name
-        }
-        else {
-            return
-            //peripheralName = "Unknown"
-        }
-        
-        let newPeripheral = Peripheral(id: peripherals.count, name: peripheralName, rssi: RSSI.intValue, peripheral: peripheral)
-        print(newPeripheral)
-        newPeripheral.peripheral.delegate = self
-        peripherals.append(newPeripheral)
+        print(peripheral)
+        selectedPeripheral = peripheral
+        selectedPeripheral.delegate = self
+        myCentral.stopScan()
+        myCentral.connect(selectedPeripheral)
+//        var peripheralName: String!
+//
+//        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+//            peripheralName = name
+//        }
+//        else {
+//            return
+//            //peripheralName = "Unknown"
+//        }
+//
+//        let newPeripheral = Peripheral(id: peripherals.count, name: peripheralName, rssi: RSSI.intValue, peripheral: peripheral)
+//        print(newPeripheral)
+//        newPeripheral.peripheral.delegate = self
+//        peripherals.append(newPeripheral)
     
     }
     @Published var peripherals = [Peripheral]()
     @Published var isSwitchedOn = false
-    @Published var entries: [CGPoint] = [CGPoint(x: 10, y: 50)]
-    var temperatures:[CGFloat] = [15,18,16]
-    var min:CGFloat = 12
-    var max:CGFloat = 20
+    @Published var entries: [CGPoint] = []// = [CGPoint(x: 10, y: 50)]
+    @Published var currentTemperature: String = "No temp yet"
+    var temperatures:[CGFloat] = []// = [15,18,16]
+    @Published var min:CGFloat = 12
+    @Published var max:CGFloat = 20
+    @Published var range:CGFloat = 8
+    @Published var isConnected:Bool = false
     var yRange:CGFloat = 200
     let screenSize: CGRect = UIScreen.main.bounds
     
@@ -43,21 +54,23 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
 
         myCentral = CBCentralManager(delegate: self, queue: nil)
         myCentral.delegate = self
-        entries.append(CGPoint(x: 20, y: 60))
-        entries.append(CGPoint(x: 30, y: 80))
+//        entries.append(CGPoint(x: 20, y: 60))
+//        entries.append(CGPoint(x: 30, y: 80))
+        range = max - min
     }
     func doCalculatePositions(){
-        max = temperatures.max()! + 2
-        min = temperatures.min()! - 2
+        max = temperatures.max()! + 1
+        min = temperatures.min()! - 1
+        range = max - min
         calculatePositions(temperatures: temperatures, max: max, min: min)
     }
     func calculatePositions(temperatures:[CGFloat], max:CGFloat, min:CGFloat) -> [CGPoint]{
         var positions:[CGPoint] = []
-        let xIncrement:CGFloat = screenSize.width/10
+        let xIncrement:CGFloat = (screenSize.width-45-10)/9
 
         for (index, temp) in temperatures.enumerated(){
             let normTemp = (1-(temp - min)/(max-min))*yRange
-            positions.append(CGPoint(x: (screenSize.width - CGFloat(index)*xIncrement),y: normTemp))
+            positions.append(CGPoint(x: (screenSize.width - CGFloat(index)*xIncrement-10),y: normTemp))
         }
         entries = positions
         return positions
@@ -72,7 +85,7 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     }
     func startScanning() {
          print("startScanning")
-         myCentral.scanForPeripherals(withServices: nil, options: nil)
+         myCentral.scanForPeripherals(withServices: [uuidService], options: nil)
      }
     func stopScanning() {
         print("stopScanning")
@@ -81,9 +94,16 @@ class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("connected to ")
         print(peripheral.name)
-        peripheral.discoverServices(nil)
+        selectedPeripheral.discoverServices(nil)
+        myCentral.stopScan()
+        isConnected = true
     }
-    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("Disconnected!")
+        isConnected = false
+        myCentral.scanForPeripherals(withServices: [uuidService])
+
+    }
 }
 extension BLEManager: CBPeripheralDelegate{
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
@@ -105,10 +125,25 @@ extension BLEManager: CBPeripheralDelegate{
                 peripheral.setNotifyValue(true, for: characteristic)
                 print("\(characteristic.uuid): properties contains .notify")
             }
-
         }
     }
     
-    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        switch characteristic.uuid {
+        case uuidCharForIndicate:
+            let data = characteristic.value ?? Data()
+            let stringValue = String(data: data, encoding: .utf8) ?? ""
+            print(stringValue)
+            currentTemperature = stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            let n = CGFloat((stringValue as NSString).floatValue)
+            temperatures.insert(n, at:0)
+            if (temperatures.count > 10){
+                temperatures.removeLast()
+            }
+            doCalculatePositions()
+        default:
+            print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        }
+    }
     
 }
--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -24,10 +24,10 @@ struct ContentView: View {
     
     @State var signInSuccess = false
     @ObservedObject var bleManager = BLEManager()
-
+    
     var body: some View {
         return Group {
-            if signInSuccess {
+            if bleManager.isConnected {
                 AppHome(bleManager: bleManager)
             }
             else {
@@ -48,26 +48,11 @@ struct ContentView_asd: View {
     @Binding var signInSuccess: Bool
 
     var body: some View {
+        
+        
+        
         VStack (spacing: 10) {
 
-            Path() { p in
-                for i in 0..<bleManager.entries.count {
-                    if (i == 0){
-                        p.move(to: CGPoint(x: bleManager.entries[i].x, y: bleManager.entries[i].y))
-                    }
-                    else{
-                        p.addLine(to: CGPoint(x: bleManager.entries[i].x, y:bleManager.entries[i].y))
-                    }
-                }
-            }.trim(from: 0, to: percentage) // << breaks path by parts, animatable
-            .stroke(Color.black, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
-            .onAppear {
-                withAnimation(.easeOut(duration: 1.0)) {self.percentage = 1.0} // <<
-            }
-            .onReceive(bleManager.$entries, perform: { _ in
-                self.percentage = .zero                // >> stops current
-                withAnimation(.easeOut(duration: 1.0)) {self.percentage = 1.0} // <<
-            })
 
             Text("Bluetooth Devices")
                 .font(.largeTitle)
@@ -91,8 +76,10 @@ struct ContentView_asd: View {
             Text("STATUS")
                 .font(.headline)
 
+
             // Status goes here
             if bleManager.isSwitchedOn {
+
                 Text("Bluetooth is switched on")
                     .foregroundColor(.green)
             }
@@ -119,6 +106,7 @@ struct ContentView_asd: View {
                 VStack (spacing: 10) {
                     Button(action: {
                         print("Start Advertising")
+                        bleManager.isConnected = true
                         self.signInSuccess = true
 
                     }) {
@@ -127,7 +115,7 @@ struct ContentView_asd: View {
                     Button(action: {
                         print("Stop Advertising")
                         bleManager.temperatures.insert(CGFloat(Int.random(in: 15...25)), at: 0)
-                        if (bleManager.temperatures.count >= 10){
+                        if (bleManager.temperatures.count > 10){
                             bleManager.temperatures.removeLast()
                         }
 //                        bleManager.temperatures.append(CGFloat(Int.random(in: 15...25)))
@@ -151,9 +139,45 @@ struct ContentView_Previews: PreviewProvider {
 struct AppHome: View {
     @ObservedObject var bleManager: BLEManager
     @State private var percentage: CGFloat = .zero
-
+    var lightGrey: Color = Color.init(red: 0.75, green: 0.75, blue: 0.75)
     var body: some View {
-        VStack {
+        VStack{
+            Text("Stroller temperature").padding(.top,40).padding(.bottom, 40).font(.largeTitle)
+            Text(bleManager.currentTemperature).padding(.top,40).padding(.bottom, 40).font(.largeTitle)        .foregroundColor((Float(bleManager.currentTemperature) ?? 0 > 18.0 ) ? .green : .red)
+
+        ZStack{
+
+            ForEach(0..<5){ n in
+            Path() { p in
+                p.move(to: CGPoint(x:n*80+45,y:0))
+                p.addLine(to: CGPoint(x:n*80+45,y:200))
+            }        .stroke(lightGrey, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+            }
+            
+            Path() { p in
+                p.move(to: CGPoint(x:40,y:0))
+                p.addLine(to: CGPoint(x:365,y:0))
+            }        .stroke(Color.gray, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+            Path() { p in
+                p.move(to: CGPoint(x:40,y:200))
+                p.addLine(to: CGPoint(x:365,y:200))
+            }        .stroke(Color.gray, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+            
+            ForEach(1..<4){ n in
+                Path() { p in
+                    p.move(to: CGPoint(x:40,y:n*50))
+                    p.addLine(to: CGPoint(x:365,y:n*50))
+                }        .stroke(lightGrey, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+            }
+            let max:CGFloat = bleManager.max
+            let range:CGFloat = bleManager.range
+            ForEach(0..<5){ n in
+                let text:String = (max-range/4*CGFloat(n)).description
+                let yPos:CGFloat = 200/CGFloat(4)*CGFloat(n)
+                Text(text).position(x:20,y:yPos)
+
+            }
+ 
             Path() { p in
                 for i in 0..<bleManager.entries.count {
                     if (i == 0){
@@ -172,18 +196,34 @@ struct AppHome: View {
                 self.percentage = .zero                // >> stops current
                 withAnimation(.easeOut(duration: 1.0)) {self.percentage = 1.0} // <<
             })
-        Text("Hello freaky world!")
-        Text("You are signed in.")
+        }.padding(.top, 40)
+
+        #if targetEnvironment(simulator)
+
         Button(action: {
                 print("Stop Advertising")
-                bleManager.temperatures.insert(CGFloat(Int.random(in: 15...25)), at: 0)
-                if (bleManager.temperatures.count >= 10){
+                let randTemp = CGFloat(Int.random(in: 15...25))
+                bleManager.currentTemperature = randTemp.description
+                bleManager.temperatures.insert(randTemp, at: 0)
+                if (bleManager.temperatures.count > 10){
                     bleManager.temperatures.removeLast()
                 }
                 bleManager.doCalculatePositions()
             }) {
                 Text("Stop Advertising")
             }
+        #endif
+        }
+    }
+}
+struct GraphLabels: View {
+    @ObservedObject var bleManager: BLEManager
+    var body: some View {
+        HStack {
+            Button(action: {}) { Text("7").font(.largeTitle) }
+            Button(action: {}) { Text("8").font(.largeTitle) }
+            Button(action: {}) { Text("9").font(.largeTitle) }
+            Button(action: {}) { Text("10").font(.largeTitle) }
         }
     }
 }
